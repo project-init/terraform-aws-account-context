12f7b24cc011e605fa161f082967fb76e2a753d0
drush211 <20804720+drush211@users.noreply.github.com>
2025-12-09 13:26:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 locals {
   # Account
-  account_id_param_name = "/aws-account/${var.aws_account_name}/account/id"
+  account_id_param_name = "/account-context/${var.aws_account_name}/account/id"
   account_id            = var.account_id == "" ? data.aws_ssm_parameter.account_id[0].insecure_value : var.account_id
 
   # Networking
-  vpc_id_param_name          = "/aws-account/${var.aws_account_name}/network/vpc/id"
+  vpc_id_param_name          = "/account-context/${var.aws_account_name}/network/vpc/id"
   vpc_id                     = var.vpc_id == "" ? data.aws_ssm_parameter.vpc_id[0].insecure_value : var.vpc_id
-  ipv4_cidr_block_param_name = "/aws-account/${var.aws_account_name}/network/vpc/ipv4_cidr_block"
+  ipv4_cidr_block_param_name = "/account-context/${var.aws_account_name}/network/vpc/ipv4_cidr_block"
   ipv4_cidr_block            = var.ipv4_cidr_block == "" ? data.aws_ssm_parameter.ipv4_cidr_block[0].insecure_value : var.ipv4_cidr_block
-  public_subnets_param_name  = "/aws-account/${var.aws_account_name}/network/public_subnets"
+  public_subnets_param_name  = "/account-context/${var.aws_account_name}/network/public_subnets"
   public_subnets             = var.public_subnet_ids == "" ? split(",", data.aws_ssm_parameter.public_subnets[0].insecure_value) : var.public_subnet_ids
-  private_subnets_param_name = "/aws-account/${var.aws_account_name}/network/private_subnets"
+  private_subnets_param_name = "/account-context/${var.aws_account_name}/network/private_subnets"
   private_subnets            = var.private_subnet_ids == "" ? split(",", data.aws_ssm_parameter.private_subnets[0].insecure_value) : var.private_subnet_ids
 }
 

655f3295fbb7889529a9ae30cf810944bbcb0f6a
Derek Rushing <20804720+drush211@users.noreply.github.com>
2025-12-09 13:43:01 -0600

    Working state where it correctly populates either outputs or the ssm parameters. (#2)
    * Testing things out.
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Working state.

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 locals {
   # Account
-  account_id_param_name = "/aws-account/${var.aws_account_name}/account/id"
+  account_id_param_name = "/account-context/${var.aws_account_name}/account/id"
   account_id            = var.account_id == "" ? data.aws_ssm_parameter.account_id[0].insecure_value : var.account_id
 
   # Networking
-  vpc_id_param_name          = "/aws-account/${var.aws_account_name}/network/vpc/id"
+  vpc_id_param_name          = "/account-context/${var.aws_account_name}/network/vpc/id"
   vpc_id                     = var.vpc_id == "" ? data.aws_ssm_parameter.vpc_id[0].insecure_value : var.vpc_id
-  ipv4_cidr_block_param_name = "/aws-account/${var.aws_account_name}/network/vpc/ipv4_cidr_block"
+  ipv4_cidr_block_param_name = "/account-context/${var.aws_account_name}/network/vpc/ipv4_cidr_block"
   ipv4_cidr_block            = var.ipv4_cidr_block == "" ? data.aws_ssm_parameter.ipv4_cidr_block[0].insecure_value : var.ipv4_cidr_block
-  public_subnets_param_name  = "/aws-account/${var.aws_account_name}/network/public_subnets"
-  public_subnets             = var.public_subnet_ids == "" ? split(",", data.aws_ssm_parameter.public_subnets[0].insecure_value) : var.public_subnet_ids
-  private_subnets_param_name = "/aws-account/${var.aws_account_name}/network/private_subnets"
-  private_subnets            = var.private_subnet_ids == "" ? split(",", data.aws_ssm_parameter.private_subnets[0].insecure_value) : var.private_subnet_ids
+  public_subnets_param_name  = "/account-context/${var.aws_account_name}/network/public_subnets"
+  public_subnets             = var.public_subnet_ids != null && length(var.public_subnet_ids) == 0 ? split(",", data.aws_ssm_parameter.public_subnets[0].insecure_value) : var.public_subnet_ids
+  private_subnets_param_name = "/account-context/${var.aws_account_name}/network/private_subnets"
+  private_subnets            = var.private_subnet_ids != null && length(var.private_subnet_ids) == 0 ? split(",", data.aws_ssm_parameter.private_subnets[0].insecure_value) : var.private_subnet_ids
 }
 
 ########################################################################################################################
@@ -24,6 +24,8 @@ data "aws_ssm_parameter" "account_id" {
 }
 
 resource "aws_ssm_parameter" "account_id" {
+  count = var.account_id != null && var.account_id != "" ? 1 : 0
+
   name  = local.account_id_param_name
   type  = "String"
   value = local.account_id
@@ -39,6 +41,8 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 
 resource "aws_ssm_parameter" "vpc_id" {
+  count = var.vpc_id != null && var.vpc_id != "" ? 1 : 0
+
   name  = local.vpc_id_param_name
   type  = "String"
   value = local.vpc_id
@@ -50,29 +54,35 @@ data "aws_ssm_parameter" "ipv4_cidr_block" {
 }
 
 resource "aws_ssm_parameter" "ipv4_cidr_block" {
+  count = var.ipv4_cidr_block != null && var.ipv4_cidr_block != "" ? 1 : 0
+
   name  = local.ipv4_cidr_block_param_name
   type  = "String"
   value = local.ipv4_cidr_block
 }
 
 data "aws_ssm_parameter" "public_subnets" {
-  count = var.public_subnet_ids == "" ? 1 : 0
+  count = var.public_subnet_ids == null || length(var.public_subnet_ids) == 0 ? 1 : 0
   name  = local.public_subnets_param_name
 }
 
 resource "aws_ssm_parameter" "public_subnets" {
+  count = var.public_subnet_ids != null && length(var.public_subnet_ids) > 0 ? 1 : 0
+
   name  = local.public_subnets_param_name
   type  = "StringList"
-  value = local.public_subnets
+  value = join(",", local.public_subnets)
 }
 
 data "aws_ssm_parameter" "private_subnets" {
-  count = var.private_subnet_ids == "" ? 1 : 0
+  count = var.private_subnet_ids == null || length(var.private_subnet_ids) == 0 ? 1 : 0
   name  = local.private_subnets_param_name
 }
 
 resource "aws_ssm_parameter" "private_subnets" {
+  count = var.private_subnet_ids != null && length(var.private_subnet_ids) > 0 ? 1 : 0
+
   name  = local.private_subnets_param_name
   type  = "StringList"
-  value = local.private_subnets
+  value = join(",", local.private_subnets)
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
+output "account_id" {
+  value       = local.account_id
+  description = "The ID of the AWS account."
+}
+
 output "vpc_id" {
   value       = local.vpc_id
   description = "The ID of the VPC for the account."
+}
+
+output "ipv4_cidr_block" {
+  value       = local.ipv4_cidr_block
+  description = "The VPC CIDR Block (IPv4) for the account."
+}
+
+output "public_subnet_ids" {
+  value       = local.public_subnets
+  description = "The IDs for the public subnets."
+}
+
+output "private_subnet_ids" {
+  value       = local.private_subnets
+  description = "The IDs for the private subnets."
 }
